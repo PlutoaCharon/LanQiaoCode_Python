918933536f909485d3e2162c432e6997de087a92
PlutoaCharon <1070481109@qq.com>
2020-02-28 11:23:25 +0800

    蓝桥杯Python实现

--- 蓝桥杯官网试题/预测身高.py
+++
@@ -1,9 +0,0 @@
-data = list(map(float, input().split()))
-
-if data[0] == 0:
-    ans = (data[1]*0.923 + data[2]) / 2
-    print("%.3f" % ans)
-else:
-    ans = (data[1] + data[2]) / 2 * 1.08
-    print("%.3f" % ans)
-
